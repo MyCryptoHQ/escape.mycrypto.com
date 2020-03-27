87819772f034706951d6a175308eeec779807e57
blurpesec <hahn.michael.f@gmail.com>
2020-03-27 13:44:01 -0700

    added eth address validation

--- src/index.js
+++ src/index.js
@@ -8,8 +8,10 @@ document.addEventListener("DOMContentLoaded", () => {
 	moonpayIFrame = document.getElementById('moonpayWidget');
 	// Can also replace color if we want
 	var currentSrc = 'https://buy.moonpay.io?apiKey=pk_live_Fi1kufUL8EflbE49vbZRKa71S2a4Y1D&colorCode=%23163150'
-	
-	if (currencyCode && walletAddress && signature) {
+	const isETHAddress = (address) => {
+		return /^(0(x|X)[a-fA-F0-9]{40})$/.test(address);
+	}
+	if (currencyCode && walletAddress && signature && isETHAddress(walletAddress)) {
 		currentSrc += '&currencyCode=' + currencyCode + '&walletAddress=' + walletAddress + '&signature=' + encodeURIComponent(signature)
 	}
 	moonpayIFrame.src = currentSrc
